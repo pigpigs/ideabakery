cfef5adf94e66e0a2035a8f69077e9c29fca2933
chong zhi wei <zhiwei_chong@mymail.sutd.edu.sg>
2013-10-29 14:28:39 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ideabakery2::Application.config.secret_token = 'b41b03c9ee81388fd1385e3d7c895d5d7d795a55b9705bf9a94b04fc6f70a4a6c46ee60ff0e479a42600c366c772b4fcb953a2dbc3c643c0c60ed941ea3fa389'
